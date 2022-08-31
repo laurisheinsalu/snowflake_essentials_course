33d773d2d4baa1ddf349a6d5293ba062be3a29f6
laurisheinsalu <112541425+laurisheinsalu@users.noreply.github.com>
2022-08-31 12:05:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+
+streamlit.header('Breakfast Menu')
+
 streamlit.text('Eggs')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
